a17400999155831577f187f483b19ba6c881e5f5
Bovvan <shamkov2323@gmail.com>
2022-04-11 11:07:51 +0300

    Lab 1 Python

---
+++ Year 1/Term 2/Programming/Python/Lab 1/Lab1.1/Lab1.1.py
@@ -0,0 +1,11 @@
+from CreateMainTXT import CreateMainTXT
+from ReadTXT import ReadTXT
+from Search import Search
+
+CreateMainTXT()
+ReadTXT("Main.txt")
+Search("Main.txt","ID.txt")
+ReadTXT("ID.txt")
+
+
+
