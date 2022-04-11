b5c33a5b7f5c1a364d3dfaa8747a5aaf2f15a618
Bovvan <shamkov2323@gmail.com>
2022-04-11 18:41:12 +0300

    Lab 2 merge_sort finished

--- Year 1/Term 2/DSA/Lab 2/main.py
+++ Year 1/Term 2/DSA/Lab 2/main.py
@@ -1,4 +1,5 @@
 import random
+from merge_sort import merge_sort
 
 
 def create_array(u, n):
@@ -12,32 +13,16 @@ def create_array(u, n):
     return arr
 
 
-def merge_sort(arr, p, q):
-    
-
-def merge(arr, p, q, r):
-    n1 = q - p + 1
-    n2 = r - q
-    arr_left = []
-    arr_right = []
-    for i in range(n1):
-        arr_left.append(p+i-1)
-    for i in range(n2):
-        arr_left.append(q+i)
-    arr_left.append(10**10)
-    arr_right.append(10**10)
-
-    i = 1
-    j = 1
-    for k in range(p, r):
-        if arr_left[i] <= arr_right[j]:
-            arr[k] = arr_left[i]
-            i += 1
-        else:
-            arr[k] = arr_right[j]
-            j += 1
+def create_array2(n):
+    arr = []
+    for i in range(n):
+        arr.append(i+1)
+    random.shuffle(arr)
+    return arr
 
 
 if __name__ == '__main__':
-    arr_main = create_array(5, 4)
+    arr_main = create_array2(25)
     print(*arr_main)
+    arr1 = merge_sort(arr_main)
+    print(*arr1)
